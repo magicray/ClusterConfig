c918519a004b0c8f5d85da918778143828962333
Bhupendra Singh <bhsingh@gmail.com>
2024-04-14 12:05:00 +0530

    Refactoring - Code is now divided into function that need to be sourced into the current shell

--- paxosdb.sh
+++ paxosdb.sh
@@ -1,117 +1,114 @@
-#!/bin/bash
+function paxosdb_read_server {
+KEY=$2
 
-NODES=$(cat $1)
-KEY=$(echo $2 | base64 -w 0)
-VERSION=$3
-
-QUORUM=$(($(echo $NODES | wc -w) / 2 + 1))
-KEY_VERSION=$(printf "%s-%014d" $KEY $VERSION)
-PROPOSAL_SEQ=$(date +%Y%m%d%H%M%S)
-
-PAXOS_DIR=PaxosDB/$(echo $KEY | md5sum | cut -c -3)
-KEY_VERSION_PROPOSAL_SEQ=$(printf "%s-%014d" $KEY_VERSION $PROPOSAL_SEQ)
-
-function read_server {
 cat << BASHSCRIPT | ssh $1 /usr/bin/bash -e
+mkdir -p PaxosDB && cd PaxosDB
 
-cd $PAXOS_DIR
-
-FILE=\$(ls | grep -E "^$KEY" | grep -v '\-00000000000000\-' | sort | tail -n 1)
+FILE=\$(ls | grep -E "^$KEY-" | grep -v '\-00000000000000\-' | sort | tail -n 1)
 
 echo BEGIN-\$FILE-\$(cat \$FILE)-END
-
 BASHSCRIPT
 }
 
 
-function paxos_server {
+function paxosdb_write_server {
+KEY=$2
+VERSION=$3
+PROPOSAL_SEQ=$4
+
 cat << BASHSCRIPT | ssh $1 /usr/bin/bash -e
+mkdir -p PaxosDB && cd PaxosDB
+touch .lockfile && exec 9>.lockfile && flock -x 9
 
-mkdir -p $PAXOS_DIR && cd $PAXOS_DIR
-touch .lockfile && exec 9>.lockfile && flock -x -n 9
+touch \$(printf "%s-%014d-%014d-%014d" $KEY 0 0 0)
 
-touch \$(printf "%s-%014d-%014d" $KEY_VERSION 0 0)
-
-OLDFILE=\$(ls | grep -E "^$KEY_VERSION" | sort | tail -n 1)
-
+OLDFILE=\$(ls | grep -E "^$KEY\-" | sort | tail -n 1)
+LATEST_VER=\$(echo \$OLDFILE | cut -d- -f2)
 PROMISED_SEQ=\$(echo \$OLDFILE | cut -d- -f3)
 ACCEPTED_SEQ=\$(echo \$OLDFILE | cut -d- -f4)
 
-# PAXOS - Promise Phase
-if [ ! $2 ] && [ $PROPOSAL_SEQ -gt \$PROMISED_SEQ ]; then
-    NEWFILE=\$(printf "%s-%014d" $KEY_VERSION_PROPOSAL_SEQ \$ACCEPTED_SEQ)
-
-    mv \$OLDFILE \$NEWFILE
-
-    echo BEGIN-\$NEWFILE-\$(cat \$NEWFILE)-END
+if [ $VERSION -lt \$LATEST_VER ]; then
+    PROMISED_SEQ=99999999999999
+    ACCEPTED_SEQ=99999999999999
 fi
 
-# PAXOS - Accept Phase
-if [ $2 ] && [ $PROPOSAL_SEQ -ge \$PROMISED_SEQ ]; then
-    NEWFILE=\$(printf "%s-%014d" $KEY_VERSION_PROPOSAL_SEQ $PROPOSAL_SEQ)
-
-    echo -n $2 > .tmp && mv .tmp \$NEWFILE
-
-    echo \$USER@\$HOSTNAME accepted \$NEWFILE
+if [ $VERSION -gt \$LATEST_VER ]; then
+    PROMISED_SEQ=00000000000000
+    ACCEPTED_SEQ=00000000000000
 fi
 
-if [ -s \$(ls -r | grep -E "^$KEY-" | head -n 1) ]; then
-    rm \$(ls -r | grep -E "^$KEY-" | tail -n +2)
+if [ $# -eq 4 ] && [ $PROPOSAL_SEQ -gt \$PROMISED_SEQ ]; then
+    FILE=\$(printf "%s-%s-%s-%s" $KEY $VERSION $PROPOSAL_SEQ \$ACCEPTED_SEQ)
+
+    mv \$OLDFILE \$FILE
+
+    echo BEGIN-\$FILE-\$(cat \$FILE)-END
 fi
 
+if [ $# -eq 5 ] && [ $PROPOSAL_SEQ -ge \$PROMISED_SEQ ]; then
+    FILE=\$(printf "%s-%s-%s-%s" $KEY $VERSION $PROPOSAL_SEQ $PROPOSAL_SEQ)
+
+    echo -n $5 > .tmp && mv .tmp \$FILE
+
+    echo \$USER@\$HOSTNAME accepted \$FILE
+
+    FLAG=0
+    for FILE in \$(ls -r | grep -E "^$KEY\-"); do
+        [ \$FLAG -gt 0 ] && rm \$FILE && echo \$USER@\$HOSTNAME deleted \$FILE
+
+        [ -s \$FILE ] && FLAG=1
+    done
+fi
 BASHSCRIPT
 }
 
 
-if [ $# -gt 2 ]; then
-    VALUE=$(base64 -w 0 -)
+function paxosdb {
+    KEY=$(echo $1 | base64 -w 0)
+    VERSION=$(printf "%014d" $2)
+
+    QUORUM=$(($(echo $PAXOSDB_CLUSTER | wc -w) / 2 + 1))
     TMPFILE=$(mktemp -p /var/tmp)
+    PROPOSAL_SEQ=$(date +%Y%m%d%H%M%S)
 
-    for NODE in $NODES; do
-        paxos_server $NODE | grep -E "^BEGIN-.+-.+-.+-.+-.*-END$"
-    done | grep $KEY_VERSION | sort > $TMPFILE
+    if [ $# -gt 1 ]; then
+        for NODE in $PAXOSDB_CLUSTER; do
+            paxosdb_write_server $NODE $KEY $VERSION $PROPOSAL_SEQ
+	done | grep -E "^BEGIN\-$KEY\-$VERSION\-.+\-.+\-.*-END$" | sort > $TMPFILE
 
-    if [ ! -s $TMPFILE ] || [ $QUORUM -gt $(cat $TMPFILE | wc -l) ]; then
-        rm $TMPFILE
-        echo NO_QUORUM
-        exit 1
-    fi
+        if [ $(cat $TMPFILE | wc -l) -ge $QUORUM ]; then
+            LASTLINE=$(tail -n 1 $TMPFILE)
+            ACCEPTED_SEQ=$(echo $LASTLINE | cut -d- -f5)
+            ACCEPTED_VALUE=$(echo $LASTLINE | cut -d- -f6)
 
-    LASTLINE=$(tail -n 1 $TMPFILE)
-    rm $TMPFILE
+            if [ $ACCEPTED_SEQ -gt 0 ]; then
+                PROPOSAL_VALUE=$ACCEPTED_VALUE
+                echo writing EXISTING value
+            else
+                PROPOSAL_VALUE=$(base64 -w 0 -)
+                echo writing NEW value
+            fi
 
-    if [ $(echo $LASTLINE | cut -d- -f5) -gt 0 ]; then
-        VALUE=$(echo $LASTLINE | cut -d- -f6)
-        echo writing EXISTING value
+            for NODE in $PAXOSDB_CLUSTER; do
+                paxosdb_write_server $NODE $KEY $VERSION $PROPOSAL_SEQ $PROPOSAL_VALUE
+            done
+        fi
     else
-        echo writing NEW value
+        echo BEGIN-$KEY-00000000000000-0-0--END > $TMPFILE
+        for NODE in $PAXOSDB_CLUSTER; do
+            paxosdb_read_server $NODE $KEY | grep -E "^BEGIN\-$KEY\-.+\-.+\-.+\-.+-END$"
+        done | sort >> $TMPFILE
+
+        KEY_VERSION=$(tail -n 1 $TMPFILE | cut -d- -f2-3)
+        COUNT_VALUE=$(cat $TMPFILE | grep $KEY_VERSION | cut -d- -f6 | uniq -c)
+
+        if [ $(echo $COUNT_VALUE | cut -d' ' -f1) -ge $QUORUM ]; then
+    	    echo KEY: $(echo $KEY_VERSION | cut -d- -f1 | base64 -d)
+            echo VERSION: $((10#$(echo $KEY_VERSION | cut -d- -f2)))
+            echo
+            echo $COUNT_VALUE | cut -d' ' -f2 | base64 -d
+        fi
     fi
 
-    for NODE in $NODES; do
-        paxos_server $NODE $VALUE
-    done
-else
-    TMPFILE=$(mktemp -p /var/tmp)
-
-    for NODE in $NODES; do
-        read_server $NODE | grep $KEY | grep -E "^BEGIN-.+-.+-.+-.+-.+-END$"
-    done | sort > $TMPFILE
-
-    if [ ! -s $TMPFILE ]; then
-        rm $TMPFILE
-        echo NOT_FOUND
-        exit 1
-    fi
-
-    KEY_VERSION=$(tail -n 1 $TMPFILE | cut -d- -f2-3)
-    VALUE=$(cat $TMPFILE | grep $KEY_VERSION | cut -d- -f6 | uniq -c)
     rm $TMPFILE
-
-    if [ $QUORUM -gt $(echo $VALUE | cut -d' ' -f1) ]; then
-        echo NO_QUORUM
-        exit 2
-    fi
-
-    echo version=$((10#$(echo $KEY_VERSION | cut -d- -f2)))
-    echo $VALUE | cut -d' ' -f2 | base64 -d
-fi
+}
