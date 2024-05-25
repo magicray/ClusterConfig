fa08adf8a929612125a1f2c4520bc32d6b867b0e
Bhupendra Singh <bhsingh@gmail.com>
2024-05-25 14:48:52 +0530

    Refactoring - Using sqlite for storing data

--- paxosdb.sh
+++ paxosdb.sh
@@ -1,112 +1,94 @@
-function paxosdb_read_server {
-# 1-NODE, 2-KEY
+#!/bin/bash -e
 
-cat << BASHSCRIPT | ssh $1 /usr/bin/bash -e
-mkdir -p PaxosDB && cd PaxosDB
+KEY=$(echo $1 | base64 -w 0)
+VERSION=$2
+QUORUM=$(($(echo $PAXOS_CLUSTER | wc -w) / 2 + 1))
 
-FILE=\$(ls | grep -E "^$2\-" | grep -v '\-00000000000000\-' | sort | tail -n 1)
 
-echo BEGIN-\$FILE-\$(cat \$FILE)-END
-BASHSCRIPT
+function promise {
+cat << SQL | ssh $1 sqlite3 ./$PAXOS_DB
+create table if not exists paxos(
+    key          text,
+    version      integer,
+    promised_seq integer,
+    accepted_seq integer,
+    value        text,
+    primary key(key, version));
+
+insert or ignore into paxos values('$2',$3,0,0,null);
+
+update paxos
+set promised_seq=$4
+where key='$2' and version=$3 and promised_seq < $4;
+
+select accepted_seq, value
+from paxos
+where key='$2' and version=$3 and promised_seq=$4;
+
+SQL
 }
 
+function accept {
+cat << SQL | ssh $1 sqlite3 ./$PAXOS_DB
 
-function paxosdb_write_server {
-# 1-NODE, 2-KEY, 3-VERSION, 4-PROPOSAL_SEQ, 5-VALUE
+update paxos
+set promised_seq=$4, accepted_seq=$4, value='$5'
+where key='$2' and version=$3 and promised_seq=$4;
 
-cat << BASHSCRIPT | ssh $1 /usr/bin/bash -e
-mkdir -p PaxosDB && cd PaxosDB
-touch .lockfile && exec 9>.lockfile && flock -x 9
+delete from paxos where key='$2' and version < (
+    select max(version) from paxos where accepted_seq is not null);
 
-touch \$(printf "%s-%014d-%014d-%014d" $2 0 0 0)
-
-OLDFILE=\$(ls | grep -E "^$2\-" | sort | tail -n 1)
-LATEST_VER=\$(echo \$OLDFILE | cut -d- -f2)
-PROMISED_SEQ=\$(echo \$OLDFILE | cut -d- -f3)
-ACCEPTED_SEQ=\$(echo \$OLDFILE | cut -d- -f4)
-
-if [ $3 -lt \$LATEST_VER ]; then
-    PROMISED_SEQ=99999999999999
-    ACCEPTED_SEQ=99999999999999
-fi
-
-if [ $3 -gt \$LATEST_VER ]; then
-    PROMISED_SEQ=00000000000000
-    ACCEPTED_SEQ=00000000000000
-fi
-
-if [ $# -eq 4 ] && [ $4 -gt \$PROMISED_SEQ ]; then
-    FILE=\$(printf "%s-%s-%s-%s" $2 $3 $4 \$ACCEPTED_SEQ)
-
-    mv \$OLDFILE \$FILE
-
-    echo BEGIN-\$FILE-\$(cat \$FILE)-END
-fi
-
-if [ $# -eq 5 ] && [ $4 -ge \$PROMISED_SEQ ]; then
-    FILE=\$(printf "%s-%s-%s-%s" $2 $3 $4 $4)
-
-    echo -n $5 > .tmp && mv .tmp \$FILE
-
-    echo \$USER@\$HOSTNAME accepted \$FILE
-
-    FLAG=0
-    for FILE in \$(ls -r | grep -E "^$2\-"); do
-        [ \$FLAG -gt 0 ] && rm \$FILE
-
-        [ -s \$FILE ] && FLAG=1
-    done
-fi
-BASHSCRIPT
+SQL
 }
 
+function fetch {
+cat << SQL | ssh $1 sqlite3 ./$PAXOS_DB
 
-function paxosdb {
-    local key=$(echo $1 | base64 -w 0)
-    local version=$(printf "%014d" $2)
+select version, accepted_seq, value
+from paxos
+where key='$2' and accepted_seq is not null
+order by version desc
+limit 1
 
-    local QUORUM=$(($(echo $PAXOSDB_CLUSTER | wc -w) / 2 + 1))
-    local tmpfile=$(mktemp -p /var/tmp)
-    local proposal_seq=$(date +%Y%m%d%H%M%S)
+SQL
+}
 
-    if [ $# -gt 1 ]; then
-        for NODE in $PAXOSDB_CLUSTER; do
-            paxosdb_write_server $NODE $key $version $proposal_seq
-	done | grep -E "^BEGIN\-$key\-$version\-.+\-.+\-.*-END$" | sort > $tmpfile
+1>&2 echo "db($PAXOS_DB) quorum($QUORUM)"
+for NODE in $PAXOS_CLUSTER; do
+    1>&2 echo "nodes($NODE)"
+done
+1>&2 echo
 
-        if [ $(cat $tmpfile | wc -l) -ge $QUORUM ]; then
-            local lastline=$(tail -n 1 $tmpfile)
-            local accepted_seq=$(echo $lastline | cut -d- -f5)
-            local accepted_value=$(echo $lastline | cut -d- -f6)
+if [ $# -eq 2 ]; then
+    PROPOSAL_SEQ=$(date +%s)
 
-            if [ $accepted_seq -gt 0 ]; then
-                local proposal_value=$accepted_value
-                echo writing EXISTING value
-            else
-                local proposal_value=$(base64 -w 0 -)
-                echo writing NEW value
-            fi
+    ACCEPTED_SEQ=0
+    ACCEPTED_VALUE=$(base64 -w 0 -)
 
-            for NODE in $PAXOSDB_CLUSTER; do
-                paxosdb_write_server $NODE $key $version $proposal_seq $proposal_value
-            done
+    for NODE in $PAXOS_CLUSTER; do
+        result=$(promise $NODE $KEY $VERSION $PROPOSAL_SEQ)
+        if [ $(echo $result | cut -d'|' -f1) -gt $ACCEPTED_SEQ ]; then
+            ACCEPTED_VALUE=$(echo $result | cut -d'|' -f2)
         fi
-    else
-        echo BEGIN-$key-00000000000000-0-0--END > $tmpfile
-        for NODE in $PAXOSDB_CLUSTER; do
-            paxosdb_read_server $NODE $key | grep -E "^BEGIN\-$key\-.+\-.+\-.+\-.+-END$"
-        done | sort >> $tmpfile
+    done
 
-        local key_version=$(tail -n 1 $tmpfile | cut -d- -f2-3)
-        local count_value=$(cat $tmpfile | grep $key_version | cut -d- -f6 | uniq -c)
+    echo "proposing for accepted_seq($ACCEPTED_SEQ) value($ACCEPTED_VALUE)"
 
-        if [ $(echo $count_value | cut -d' ' -f1) -ge $QUORUM ]; then
-            echo KEY: $(echo $key_version | cut -d- -f1 | base64 -d)
-            echo VERSION: $((10#$(echo $key_version | cut -d- -f2)))
-            echo
-            echo $count_value | cut -d' ' -f2 | base64 -d
+    for NODE in $PAXOS_CLUSTER; do
+        accept $NODE $KEY $VERSION $PROPOSAL_SEQ $ACCEPTED_VALUE
+    done
+elif [ $# -eq 1 ]; then
+    RESULT=$(for NODE in $PAXOS_CLUSTER; do
+                 fetch $NODE $KEY
+             done | sort | uniq -c)
+
+    if [ $RESULT ] && [ $(echo $RESULT | wc -l) -eq 1 ]; then
+        if [ $(echo $RESULT | cut -d' ' -f1) -ge $QUORUM ]; then
+            ROW=$(echo $RESULT | cut -d' ' -f2)
+            VERSION=$(echo $ROW | cut -d'|' -f1)
+            VALUE=$(echo $ROW | cut -d'|' -f3)
+            echo $VERSION
+            echo $VALUE | base64 -d
         fi
     fi
-
-    rm $tmpfile
-}
+fi
